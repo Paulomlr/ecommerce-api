564fbf3570993cd20e3e51777d990a273fd26f79
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-09-17 14:55:21 -0300

    feat add CategoryRequestDTO

---
+++ src/main/java/com/ecommerce_project/dtos/CategoryRequestDTO.java
@@ -0,0 +1,13 @@
+package com.ecommerce_project.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CategoryRequestDTO(
+        @NotBlank
+        @Size(min = 3, message = "Category name must be at least 3 characters long")
+        String categoryName) {
+    public CategoryRequestDTO {
+        categoryName = categoryName.toUpperCase();
+    }
+}
