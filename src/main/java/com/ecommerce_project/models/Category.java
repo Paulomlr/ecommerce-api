f150f3503a1fabd16fb11ff6622ee25b05f61a5b
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-09-17 14:45:15 -0300

    feat: add category model

---
+++ src/main/java/com/ecommerce_project/models/Category.java
@@ -0,0 +1,21 @@
+package com.ecommerce_project.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    private String categoryName;
+}
