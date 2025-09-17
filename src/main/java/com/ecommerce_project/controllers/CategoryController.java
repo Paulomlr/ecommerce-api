3482277e2f7b0f5632925b435301991e6c051285
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-09-17 15:49:18 -0300

    feat: add CategoryController

---
+++ src/main/java/com/ecommerce_project/controllers/CategoryController.java
@@ -0,0 +1,46 @@
+package com.ecommerce_project.controllers;
+
+import com.ecommerce_project.dtos.CategoryRequestDTO;
+import com.ecommerce_project.dtos.CategoryResponseDTO;
+import com.ecommerce_project.dtos.PaginatedCategoryResponseDTO;
+import com.ecommerce_project.services.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    public final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<PaginatedCategoryResponseDTO> getAllCategories() {
+        var allCategories = categoryService.getAllCategories();
+        return new ResponseEntity<>(allCategories, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponseDTO> createCategory(@RequestBody @Valid CategoryRequestDTO categoryRequestDTO) {
+        var category = categoryService.createCategory(categoryRequestDTO);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{categoryId}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Long categoryId) {
+        categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryResponseDTO>updateCategory(@PathVariable Long categoryId,
+                                                             @RequestBody @Valid CategoryRequestDTO categoryRequestDTO) {
+        var category = categoryService.updateCategory(categoryRequestDTO, categoryId);
+        return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+}
