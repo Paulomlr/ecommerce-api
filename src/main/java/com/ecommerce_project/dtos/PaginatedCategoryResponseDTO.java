cffba0afd5ff4ad70f42b59f2df57081303f0f40
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-09-19 21:39:36 -0300

    feat: add PaginatedCategoryResponseDTO for paginated responses

--- src/main/java/com/ecommerce_project/dtos/PaginatedCategoryResponseDTO.java
+++ src/main/java/com/ecommerce_project/dtos/PaginatedCategoryResponseDTO.java
@@ -2,5 +2,10 @@ package com.ecommerce_project.dtos;
 
 import java.util.List;
 
-public record PaginatedCategoryResponseDTO(List<CategoryResponseDTO> categories) {
+public record PaginatedCategoryResponseDTO(List<CategoryResponseDTO> categories,
+                                           Integer pageNumber,
+                                           Integer pageSize,
+                                           Long totalElements,
+                                           Integer totalPages,
+                                           Boolean lastPage) {
 }
